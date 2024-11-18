2a9d58afc6f479a194fdb6c517417531bc59c5b8
Muhammad Saad Hussain <saadh7010@gmail.com>
2024-11-18 17:33:38 +0500

    trajnsactional

--- order-service/src/main/java/com/aimnode/order_service/service/OrderService.java
+++ order-service/src/main/java/com/aimnode/order_service/service/OrderService.java
@@ -7,11 +7,13 @@ import com.aimnode.order_service.model.OrderLineItems;
 import com.aimnode.order_service.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class OrderService {
 
     @Autowired
